12f4752d34d7a84c06826c432077666c59be13df
Deo Valiandro. M <valiandrod@gmail.com>
2024-10-09 09:35:47 +0800

    number

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use core::f32;
+use std::i32;
+
 fn main() {
     println!("Hello, world!");
 }
@@ -31,4 +34,15 @@ fn shadowing() {
 
     let r0: &str = "Ok";
     println!("{r0}");
+}
+
+#[test]
+fn number() {
+    let i: u32 = u32::MAX;
+    let j: i32 = i32::MAX;
+    let k: f32 = f32::MAX;
+
+    println!("MAX u32 = {i}");
+    println!("MAX i32 = {j}");
+    println!("MAX f32 = {k}");
 }
