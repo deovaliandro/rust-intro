96fc02051aee76814b6480f5e711326f758fbb7a
Deo Valiandro. M <valiandrod@gmail.com>
2024-10-09 09:27:34 +0800

    variabel, mutable variabel, shadowing

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,32 @@ fn main() {
 
 
 #[test]
-fn hello_unit_test() {
+fn unit_test() {
     println!("Hello unit test rust");
+}
+
+#[test]
+fn variable_test() {
+    let name: &str = "Deo";
+    println!("{name}");
+}
+
+#[test]
+fn mut_variable_test() {
+    let mut name: &str = "Ini pertama";
+    println!("{name}");
+
+    name = "Diubah lagi lah";
+
+    println!("{name}");
+}
+
+// shadowing tidak disarankan
+#[test]
+fn shadowing() {
+    let r0: i32 = 1;
+    println!("{r0}");
+
+    let r0: &str = "Ok";
+    println!("{r0}");
 }
